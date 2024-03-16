710cd3252aac588239a8bfdb98bb249b2a946844
Noob <yurukute@gmail.com>
2024-03-16 16:32:37 +0000

    customize profile
    - add background
    - add typed.js
    - create custom css

---
+++ assets/js/typed.js
@@ -0,0 +1,6 @@
+new Typed('#typed', {
+    strings: ["I am a fresh graduate Software Engineer", "I love embedded systems", "I also blog, Noob is my pen name"],
+    typeSpeed: 30,
+    backSpeed: 40,
+    loop: true
+});
