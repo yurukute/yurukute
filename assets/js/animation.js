8c1056491abd06e9c5f4907f0573fa36ae0af094
yurukute <yurukute@gmail.com>
2024-03-31 03:06:32 +0700

    add animations

---
+++ assets/js/animation.js
@@ -0,0 +1,29 @@
+const trigger = (window.innerHeight / 5) * 4;
+const main = document.getElementsByTagName("main")[0]
+
+main.addEventListener("scroll", () => {
+    showContent(".details", ["animate__fadeInRight", "animate__fadeInLeft"]);
+    showContent(".project-item", "animate__fadeInUp")
+}, false);
+
+function showContent(selector, animations) {
+    if (Array.isArray(animations) && animations.length == 2) {
+        var odd_animation = animations[0];
+        var even_animation = animations[1];
+    }
+    else odd_animation = even_animation = animations;
+
+    let boxes = document.querySelectorAll(selector);
+    let delay = 200;
+
+    boxes.forEach((box, idx) => {
+        let top = box.getBoundingClientRect().top;
+        let animation = (idx % 2) ? odd_animation : even_animation;
+        if (top < trigger) {
+            setTimeout(() => {
+                box.classList.add("animate__animated", animation);
+            }, delay);
+            delay += 200;
+        }
+    });
+}
